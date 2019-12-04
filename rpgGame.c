687d522aeff4d7c57d9d30f56e459bf9e88212dc
abe <abrahamcelis@live.com>
2019-12-04 10:21:25 -0800

    Final Version 2.0

--- rpgGame.c
+++ rpgGame.c
@@ -171,114 +171,164 @@ void main(void)
 							
 							else if(choice == 3)
 							{
-								puts("Your search ends here, you seem to have searched far and wide. I'll tell you the treasure is hidden further inside. The dungeon is filled withvrooms, your search is almost over.\n");
-								puts("If you can answer correctly I'll tell you where your treasure lies.\n");
-								puts("Let's start off easy... If a woman has 20 watermelons in her cart but needs to get 10 more how many will she have?");
-								puts("Honestly, I'm hunting treasure so I don't have to do stuff like this... The cat just stares. You answer: ");
+								puts("Your search ends here, you seem to have searched far and wide, I'll tell you where the treasure is hidden.");
+								puts("It's within this dungoen, but with so many rooms you will have to get through me to find it.");
+								puts("Just answer correctly.");
+								puts("This should be easy... if a woman has 20 watermelons, but needs 10 more how many will she then have?");
+								puts("You tell her you're here to hunt for traesure not answer math problems! Honestly I'm hunting treasure so I don't have to do stuff like this... The cat stares unamused. You answer: ");
+								puts("SYSTEM MESSAGE: TYPE IN A DIGIT");
 								scanf("%d", &watermelon);
-								if(watermelon == 30){
-									puts("Correct! Although if you had failed that I doubt you would know how much treasure is in here... she mumbles.\n Let's continue!\n");
-									puts("I like games. She states. Solve this riddle from one of my favorites.\n");
-									puts("What is the beginning of eternity, the end of time and space, the beginning of every end and the end of every race?\n");
-									puts("Here's a clue, the answer is only 1 charachter long... good luck.");
-									puts("SYSTEM MESSAGE: ANSWER MUST BE LOWERCASE.");
-									scanf("%s", &ch);
-									if(ch == 'e'){
-										puts("Correct! Now I'm impressed. Just one last little thing, if you get this than we are done.");
-										puts("Just choose correctly and you win. There is a diamond at the center of the next room it shines bright but the room is filled with traps. You have a backpack on, it's heavy. There are three paths to the diamond, one to the left, one to the right, and down the center.\nThe center seems clear, but the easy path isn't always the best...\nThe path on the left seems to have a pit of spikes you would have to jump, a thin path that requires great balance, and then a rope(that seems old) that dangles from the roof that you need to swing from in order to reach the jewel.\nThe path on the right is like a speed test you need to hit a button that opens a gate across the room(if you don't get to it in time that door will never open again) if you make it there is a guard you must sneak past, and a room full of mines that will end you journey if you aren't careful.\nSo which path do you choose. Path 1(Left), 2(Center), or 3(Right).");
-										puts("SYSTEM MESSAGE: ANSWER MUST BE EITHER 1, 2, OR 3.");
-										scanf("%d", &path);
-										if(path == 1){
-											puts("The left path it is. The journey ahead seems like it requires precision do you keep your backpack on? Yes or no?");
-											puts("SYSTEM MESSAGE: 0 FOR NO, 1 FOR YES.");
-											scanf("%d", &pack);
-											if(pack ==0){
-												puts("You throw your bag aside and continue on\n");
-												puts("Without the bag weighing you down you quickly clear the gap and easily keep balance on the wall, the rope doesn't break because you're light and nimble. The jewel is yours.\n");
-												puts("Congragualtions! Your treasure awaits just beyond this door.\nYou walk in to see more gold than even a king has ever lied their eyes on.");
-											}
-											else if(pack == 1){
-												puts("Good luck. You approach the jump and well what do you know your way too heavy, your stubborness has landed you in a pit of spikes.\n");
-												puts("Wrong answer! Those who are greedy are not worthy of the treasure that I hold.\n");
-											}
-										}
-										else if(path == 2){
-											puts("I'll tell you now you chose... poorly. You walk down the center path and fall through the false floor, plumetting down a fall not even a god could survive...\n");
-											puts("Wrong answer! Those who are not willing to work for it are not deserving of it.\n");
-										}
-										else if(path == 3){
-											puts("The RIGHT path it is. Your bag might weigh you down during this test of speed what do you do? Keep it on or take it off.\n");
-											puts("SYSTEM MESSAGE: ENTER 0 TO KEEP IT ON, 1 TO TAKE IT OFF.\n");
-											scanf("%d", &pack);
-											if(pack == 0){
-												puts("You chose to keep it on interesting...\n");
-												puts("Before hitting the button you realize the bag will weigh you down. You think maybe you should throw it across before hitting the button, but maybe risking it is best.\n");
-												puts("Do you throw it across or risk it and keep it on.\n");
-												puts("SYSTEM MESSAGE: 0 TO THROW IT, 1 TO KEEP IT\n");
-												scanf("%d", &keep);
-												if(keep == 1){
-													puts("Lets Hope luck is on your side... This could go either way.\n");
-													int numgen();
-													if(ran == 0){
-														puts("Success! Looks like you just made it throgh with that pesky bag.\n");
-														puts("You enter a room with a guard pacing back and forth, you can sneek past or throw your bag as a distraction.\n");
-														puts("SYSTEM MESSAGE: TO SNEEK ENTER 0, TO DISTRACT ENTER 1.\n");
-														scanf("%d", &bag);
-														if(bag == 0){
-															puts("You try to sneek by, but you bag gives you away... you got caught!\n");
-															puts("Looks like you aren't worthy. Those who are careless are bound to fail.\n");
-														}
-														else if(bag == 1){
-															puts("You throw your bag across the room! The guard rushes over to see what caused the commotion, you see your chance and run past entering the next room. You quickly manuever past the mine and retrieve the jewel... THE TREASURE IS YOURS!\n");
-															puts("Your treasure awaits you adventurer.\nYou enter the room to see treaseure stacked so high even the moon seems closer.\n");
-														}
-													}
-													else if(ran != 0){
-														puts("Looks like you aren't as lucky as you look. The gate closes before you can get past it. Your journey ends here.\n");
-													}
-												}
-												else if(keep == 0){
-													puts("You throw it and make it across just in time. You enter a room with a guard pacing back and forth, do you sneek past or run?\n");
-													puts("SYSTEM MESSAGE: 0 TO RUN, 1 TO SNEEK");
-													scanf("%d", &guard);
-													if(guard == 0){
-														puts("You run past but in your haste you trip and land right on the mines.\n");
-														puts("Looks like your search ends here. Those who are not willing to wait are not rewarded.\n");
-												}
-													else if(guard == 1){
-														puts("Success! You sneek past with no issues and the mines aren't a problem... the jewels are yours!\n");
-														puts("You enter the room and see so much treasure you think even the sea isn't this vast.\n");
-														puts("Looks like your journey has come to a satisfying end.");
-													}
+									
+									if(watermelon == 30)
+									{
+										puts("Correct! Although if you had failed that I doubt you would even know how much treasure is in here... she mumbles.");
+										puts("I like games she states. Solve this riddle from one of my favorites.");
+										puts("What is the beginning of eternity, the end of time and space, the beginning of every end and the end of every race?");
+										puts("Here's a clue, the answer is only one character long... good luck.");
+										puts("SYSTEM MESSAGE: ANSWER MUST BE LOWERCASE");
 
-											}
-											if(pack == 1){
-												puts("You throw it to the side, hit the button and run! Your speed is incredible you clear the gate with time to spare...\n");
-												puts("You see a guard pacing back and forth. Do you sneek past or run by?\n");
-												puts("SYSTEM MESSAGE: 0 TO SNEEK BY, 1 TO RUN\n");
-												scanf("%d", &guard);
-												if(guard == 0){
-													puts("Success! You sneek past with no issues. And the mines aren't a problem the jewel is yours!\n");
-													puts("Looks like you win stranger. Your treasure awaits you just beyond this door.\n");
-													puts("You enter the door and see the treasure is so massive it will keep you family fed throgh the ages.\n");
-												}
-												else if(guard == 1){
-													puts("Honestly you're too hasty... you run past the guard only to trip and land right on the mines.\n");
-													puts("Those who are unwilling to wait for it cannot be rewarded.\nYou fail.");
-												}
-											}
-										}
-									}
-									else if(ch != 'e'){
-										puts("Got to admit that one is a bit tricky, maybe next time. For now you're not worthy.");
-									}
-								}
-								else if(watermelon != 30){
-									puts("Just get out.\n");
-								}
+										scanf("%s", &ch);
+											if(ch == 'e')
+											{
+												puts("Correct! Now I'm impressed. Just one last litte thing. If you get this we are done.");
+												puts("Now just choose your path, what you decide will determine your fate.");
+												puts("Step into my dillusion and see what I mean. You enter a strange looking portal.");
+												puts("There is a diamond at the center of the next room, it illuminates everything around it, but the there are three paths filled with traps.");
+												puts("You have a backpack on, it's heavy. You see three paths to the diamond. One to the left, one to the center, and one to the right.");
+												puts("The center path seems clear, but the easy path isn't always the best...");
+												puts("The path to the left has a pit of spikes you would have to jump, then a thin path that requires great balance, and then a rope(that seems old) that dangles from the roof that you need to swing from in order to reach the jewel.");
+												puts("The path on the right is like a speed test you need to hit a button that opens a gate across the room(if you don't make it in time the door will never open again) if you make it there, there is a guard you must get past, and a room full of mines that will end your journey if you're not careful.");
+												puts("So... what path do you choose?");
+												puts("SYSTEM MESSAGE: CHOOSE 1 FOR LEFT PATH, 2 FOR CENTER, AND 3 FOR RIGHT PATH.");
 
-							}
+												scanf("%d", &path);
+
+													if(path == 1)
+													{
+														puts("The left path it is. The journey ahead requires precision do you keep your backpack on?");
+														puts("SYSTEM MESSAGE: 0 FOR NO, 1 FOR YES");
+
+														scanf("%d", &pack);
+
+															if(pack == 0)
+															{
+																puts("You throw it to the side and continue on.");
+																puts("Without the bag weighing you down you quickly clear the gap. And easily keep balance on the path, and the rope you swing from doesn't breake because you are light and nimble. The jewel is yours.");
+																puts("Congragualtions! I see you have returned safely. Your treasures awaits just beyond this door. ");
+																puts("You walk through the door and see more gold than even a king has ever laid his eyes on.");
+															}
+															else if(pack == 1)
+															{
+																puts("Good luck. You approach the jump and to no one's surprise you're way too heavy, your stuborness has landed you in a pit of spikes.");
+																puts("The cat seess time has passed and you have not returned. You failed she says. Those who are greedy are not worthy of the treasure that I hold, she wispers.");
+															}
+													}
+											
+									
 							
+													else if(path == 2)
+													{
+														puts("The cat sees the path you have chosen and as you begin to walk down it she states you chose poorly.");
+														puts("You take a few steps and the falsse floor activates and you fall down a hole so deep even a god couldn't survive it...");
+														puts("Those who are not willing to work for it are not worthy of any reward, she whispers.");
+													}
+													else if(path == 3)
+													{
+														puts("The RIGHT path it is. Your bag might weigh you down during this test of speed, what do you do?");
+														puts("SYSTEM MESSAGE: 0 TO KEEP IT ON, 1 TO TAKE IT OFF.");
+														scanf("%d", &pack);
+
+															if(pack == 0)
+															{
+																puts("You choose to keep it on, interesting...");
+																puts("Before you hit the button you think the bag will weigh you down. You think to throw it across the room, but are unsure if it's worth the risk.");
+																puts("Do you throw it across or risk it and keep it on?");
+																puts("SYSTEM MESSAGE: 1 TO KEEP IT, 0 TO THROW IT");
+
+																scanf("%d", &keep);
+
+																	if(keep == 1)
+																	{
+																		puts("Let's hope luck is on your side... this could go either way.");
+																		int numgen();
+																		
+																			if(ran == 0)
+																			{
+																				puts("Success! Looks like you just made it through even with that pesky bag.");
+																				puts("You enter a room with a guard pacing back and forth, you can sneek past or throw your bag as a distraction.");
+																				puts("SYSTEM MESSAGE: ENTER 0 TO SNEEK, 1 TO DISTRACT.");
+																				scanf("%d", &bag);
+
+																					if(bag == 0)
+																					{
+																						puts("You try to sneek by, but your bag gives you away... you got caught!");
+																						puts("The cat sees your failure and says... you were close, but those who are careless are bound to fail.");
+																					}
+																					else if(bag == 1)
+																					{
+																						puts("You throw your bag across the room! The guard rushes over to see the commotion, you see your chance and run past entering the next room. You quickly manuever past the mines and retrieve the jewel... THE TREASURE IS YOURS!");
+																						puts("Your treasure awaits you adventerer says the cat upon your return.");
+																						puts("You enter the room to see treasure stacked so high even the moon seems closer.");
+																					}
+																			}
+																			else if(ran != 0)
+																			{
+																				puts("Looks like you're not as lucky as you seem. The gate closes before you can get through it... your journey ends here.");
+																			}
+																			
+																	}
+																	else if(keep == 0)
+																	{
+																		puts("You throw it and make it across just in time.");
+																		puts("You enter a room with a guard pacing back and forth, do you sneek or run by?");
+																		puts("SYSTEM MESSAGE: 0 TO RUN, 1 TO SNEEK");
+																		scanf("%d", &guard);
+
+																			if(guard == 0)
+																			{
+																				puts("You run past but in your haste you trip and land right on the mines.");
+																				puts("Your search ends here. Those who are impatience are not rewarded.");
+																			}
+																			else if(guard == 1)
+																			{
+																				puts("Success! You sneek past with no issues and the mines aren't a problem... the jewels are yous!");
+																				puts("You enter the room and see so much treasure you think even the sea isn't this vast.");
+																				puts("Looks like your journey has come to a satisfying end.");
+																			}
+																	}
+															}
+															else if(pack == 1)
+															{
+																puts("You throw it to the side, hit the button, and run! Your speed is so incredible you clear the gate with time to spare! But all that running has tired you out...");
+																puts("You see a guard pacing back and forth. Do you sneek past or run?");
+																puts("SYSTEM MESSAGE: 0 TO SNEEK BY, 1 TO RUN");
+																scanf("%d", &guard);
+
+																	if(guard == 0)
+																	{
+																		puts("Success! You sneek past with no issues, the mines aren't a problem, and the jewel is yours!");
+																		puts("Looks like you win stranger. Your treasure awaits you just beyond this door.");
+																		puts("You open the door and see treasure so massive it will keep your family fed for generations to come.");
+																	}
+																	else if(guard == 1)
+																	{
+																		puts("Honestly... your too hasty. You run past the guard onlt to trip and land right on the mines.");
+																		puts("Those who are unwilling to wait for it cannot be rewarded.");
+																	}
+															}
+													}
+											}
+											else if(ch != 'e')
+											{
+												puts("Got to admit that one is a bit tricky, maybe next time. For now you're not worthy.");
+											}
+									
+
+									else if(watermelon != 30)
+									{
+										puts("Just get out.");
+									}
+							}
 							else if(choice == 4)
 							{
 								puts("If you're just going to stand there I'll put you to work. Read me my favorite story..");
